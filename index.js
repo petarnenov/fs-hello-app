80e52262e4b039e038267f8ccd90caa221e4e0aa
Petar Nenov <petarnenovpetrov@gmail.com>
2023-01-29 11:40:47 +0200

    add more messages

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ console.log("first commit");
 console.log("second commit");
 console.log("third commit");
 console.log("fourth commit");
+console.log("fifth commit");
+console.log("sixth commit");
+console.log("seventh commit");
+
